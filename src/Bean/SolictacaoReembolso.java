87c11334ac2f607394f2ca538da749572ee92853
bruno <email.brunoribeiro@gmail.com>
2020-09-09 11:25:18 -0300

    Vai

---
+++ src/Bean/SolictacaoReembolso.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author bruno
+ */
+public class SolictacaoReembolso {
+    String codigo;
+    String nome;
+    BigDecimal valor;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+     
+    
+    
+    
+}
