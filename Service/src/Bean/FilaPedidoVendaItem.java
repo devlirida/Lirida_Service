9ca930d044b326c980f36d38044b4d5875f09cd5
Bruno <email.brunoribeiro@gmail.com>
2020-12-10 00:12:16 -0200

    Vai

---
+++ Service/src/Bean/FilaPedidoVendaItem.java
@@ -0,0 +1,206 @@
+
+/***********************************************************************************/
+/*CRIADO DE FORMA AUTOMATICA PELO GERADOR DE CÓDIGO DA CROSSYSTEM*/
+/*Data:  2020-06-05 */
+/***********************************************************************************/
+package Bean;
+
+import java.util.Date;
+
+public class FilaPedidoVendaItem {
+
+private Long seqFilaPedidoVendaItem;
+private Long seqUsuario;
+private String codigo;
+private String codigoFilaPedidoVenda;
+private String emailUsuario;
+private Date dataCadastro;
+private String situacao;
+private String situacaoDescricao;
+private String tag01;
+private String tag02;
+private String tag03;
+private String tag04;
+private String tag05;
+private String tag06;
+private String tag07;
+private String tag08;
+private String tag09;
+private String tag10;
+private String linha1;
+private String linha2;
+private String linha3;
+
+// Criar GETs e SETs
+
+    public Long getSeqFilaPedidoVendaItem() {
+        return seqFilaPedidoVendaItem;
+    }
+
+    public void setSeqFilaPedidoVendaItem(Long seqFilaPedidoVendaItem) {
+        this.seqFilaPedidoVendaItem = seqFilaPedidoVendaItem;
+    }
+
+    public Long getSeqUsuario() {
+        return seqUsuario;
+    }
+
+    public void setSeqUsuario(Long seqUsuario) {
+        this.seqUsuario = seqUsuario;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigoFilaPedidoVenda() {
+        return codigoFilaPedidoVenda;
+    }
+
+    public void setCodigoFilaPedidoVenda(String codigoFilaPedidoVenda) {
+        this.codigoFilaPedidoVenda = codigoFilaPedidoVenda;
+    }
+
+    public String getEmailUsuario() {
+        return emailUsuario;
+    }
+
+    public void setEmailUsuario(String emailUsuario) {
+        this.emailUsuario = emailUsuario;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public String getSituacaoDescricao() {
+        return situacaoDescricao;
+    }
+
+    public void setSituacaoDescricao(String situacaoDescricao) {
+        this.situacaoDescricao = situacaoDescricao;
+    }
+
+    public String getTag01() {
+        return tag01;
+    }
+
+    public void setTag01(String tag01) {
+        this.tag01 = tag01;
+    }
+
+    public String getTag02() {
+        return tag02;
+    }
+
+    public void setTag02(String tag02) {
+        this.tag02 = tag02;
+    }
+
+    public String getTag03() {
+        return tag03;
+    }
+
+    public void setTag03(String tag03) {
+        this.tag03 = tag03;
+    }
+
+    public String getTag04() {
+        return tag04;
+    }
+
+    public void setTag04(String tag04) {
+        this.tag04 = tag04;
+    }
+
+    public String getTag05() {
+        return tag05;
+    }
+
+    public void setTag05(String tag05) {
+        this.tag05 = tag05;
+    }
+
+    public String getTag06() {
+        return tag06;
+    }
+
+    public void setTag06(String tag06) {
+        this.tag06 = tag06;
+    }
+
+    public String getTag07() {
+        return tag07;
+    }
+
+    public void setTag07(String tag07) {
+        this.tag07 = tag07;
+    }
+
+    public String getTag08() {
+        return tag08;
+    }
+
+    public void setTag08(String tag08) {
+        this.tag08 = tag08;
+    }
+
+    public String getTag09() {
+        return tag09;
+    }
+
+    public void setTag09(String tag09) {
+        this.tag09 = tag09;
+    }
+
+    public String getTag10() {
+        return tag10;
+    }
+
+    public void setTag10(String tag10) {
+        this.tag10 = tag10;
+    }
+
+    public String getLinha1() {
+        return linha1;
+    }
+
+    public void setLinha1(String linha1) {
+        this.linha1 = linha1;
+    }
+
+    public String getLinha2() {
+        return linha2;
+    }
+
+    public void setLinha2(String linha2) {
+        this.linha2 = linha2;
+    }
+
+    public String getLinha3() {
+        return linha3;
+    }
+
+    public void setLinha3(String linha3) {
+        this.linha3 = linha3;
+    }
+
+
+
+}
